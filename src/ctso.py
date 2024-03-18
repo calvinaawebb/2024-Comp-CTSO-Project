01440ff3d0826c3ab385101ed7de732197ec08a8
FluxChikin <calvinaawebb@gmail.com>
2024-03-17 21:47:53 -0600

    Update ctso.py

--- src/ctso.py
+++ src/ctso.py
@@ -63,7 +63,7 @@ class main():
         # Menu:
         self.open_File = None
         self.savedFile = True
-        menubar = Menu(root, background=darker_grey)
+        menubar = Menu(root, background='blue', fg='white')
         root.config(menu=menubar)
         menubar.config(fg="white", activebackground=darker_grey, activeforeground="white")
 
@@ -83,13 +83,7 @@ class main():
                              tabs=('0.3i', '0.8i', RIGHT, '1.2i', CENTER, '2i', NUMERIC))
         self.mainArea.place(x=int(width * 0.12), y=50, width=int(width * 0.88), height=int(height))
 
-        can.create_text(285, 25, text="Edit files:", fill="dark gray", font=('Helvetica 15 bold'))
-
-        # Create an instance of Tkinter frame
-        win = Tk()
-
-        # Set the geometry of Tkinter frame
-        win.geometry("750x270")
+        can.create_text(245, 25, text="Edit files:", fill="light gray", font=('Helvetica 15 bold'))
 
         # scrollbar
         vert = Scrollbar(root, orient='vertical', command=self.mainArea.yview)
@@ -115,8 +109,8 @@ class main():
                     bd=0, borderwidth=0)
         menubar.add_cascade(menu=file, label="File")
         file.add_command(label="New Comment File", command=self.newFile)
-        file.add_command(label="New Meeting File", command=self.newFile)
-        file.add_command(label="New Company Information File", command=self.newFile)
+        file.add_command(label="New Meeting File", command=self.newMeetingFile)
+        file.add_command(label="New Company Information File", command=self.newCompanyFile)
         file.add_command(label="Save File", command=self.saveFile)
         file.add_command(label="Open Directory", command=self.open)
         file.add_command(label="Exit", command=self.quit)
@@ -125,15 +119,16 @@ class main():
         window = Menu(menubar, bg=darker_grey, fg="white", activebackground=light_purple, activeforeground=darker_grey,
                       bd=0)
         menubar.add_cascade(menu=window, label="Window")
-        window.add_command(label="Help", command=self.help())
-        window.add_command(label="Search Current Directory", command=self.searchDir())
+        window.add_command(label="Search Current Directory", command=self.open_search_window)
         #window.add_command(label="Time Line")
 
         # setting menu
         setting = Menu(menubar, bg=darker_grey, fg="white", activebackground=light_purple, activeforeground=darker_grey,
                        bd=0)
-        menubar.add_cascade(menu=setting, label="Setting")
-        setting.add_command(label="Preferences")
+        menubar.add_cascade(menu=setting, label="Help")
+        setting.add_command(label="Getting Started", command=self.open_popup)
+        setting.add_command(label="Q&A", command=self.open_qa)
+        setting.add_command(label="Instructions", command=self.open_inst)
 
         if width == root.winfo_width():
             print("yes")
@@ -246,18 +241,46 @@ class main():
     def newCompanyFile(self):
         newFile = filedialog.asksaveasfile(filetypes=(("text document", "*.txt"),))
         main.setDir(savePath, None, 0)
-        print(newFile)
+        # Lines to add
+        new_lines = [
+            "Company Name:",
+            "Type of Company:",
+            "Resources:",
+            "Contact Info:"
+        ]
+
+        # Open the file in append mode and add the new lines
+        with open(newFile.name, 'a') as file:
+            # Writing each line
+            for line in new_lines:
+                file.write(line + '\n')  # Add a newline character at the end of each line
 
     def newMeetingFile(self):
         newFile = filedialog.asksaveasfile(filetypes=(("text document", "*.txt"),))
         main.setDir(savePath, None, 0)
-        print(newFile)
+        # Lines to add
+        new_lines = [
+            "Meeting Name:",
+            "Type of Meeting:",
+            "Meeting Time:",
+            "Connection Info:"
+        ]
+
+        # Open the file in append mode and add the new lines
+        with open(newFile.name, 'a') as file:
+            # Writing each line
+            for line in new_lines:
+                file.write(line + '\n')  # Add a newline character at the end of each line
 
     def searchDir(self):
-         for k in files:
-             with open(pathlib.Path(k)) as f:
-                 if 'hello' in f.read():
-                     print("LETS GOOOOOO")
+        for subdir, dirs, files in os.walk(savePath):
+            for file in files:
+                print(os.path.join(subdir, file))
+                with open(os.path.join(subdir, file)) as f:
+                    if entry_search.get() in f.read():
+                        pop = Label(root, text=file)
+                        pop.pack()
+                        root.after(2500, lambda: pop.pack_forget())
         #print("PathK: ")
         #print(files[0])
 
@@ -266,12 +289,166 @@ class main():
         helpm.pack()
         root.after(5000, lambda: helpm.pack_forget())
 
-    def open_popup():
-        top = Toplevel(win)
-        top.geometry("750x250")
-        top.title("Child Window")
+    def open_search_window(self):
+        # Create Toplevel window
+        search_window = Toplevel(root)
+        search_window.title("Search")
+        search_window.resizable(False, False)
+        search_window.grab_set()
+        search_window.focus_set()
+        search_window.config(bg='#55555a')
+
+        # Set size and position of the window
+        search_window.geometry("300x100")
+
+        # Create and place entry and button widgets
+        global entry_search
+        entry_search = Entry(search_window, width=30, font=('Helvetica', 12))
+        entry_search.pack(pady=10, padx=10)
+
+        btn_search = Button(search_window, text="Search", command=self.searchDir, font=('Helvetica', 12),  bg='#55555a', fg='white', relief=FLAT)
+        btn_search.pack(pady=5)
+
+    def open_popup(self):
+        global top
+        top = Toplevel(root)
+        top.geometry("600x350")
+        top.resizable(False, False)
+        top.grab_set()
+        top.focus_set()
+        top.title("Help Menu")
         Label(top, text="Hello World!", font=('Mistral 18 bold')).place(x=150, y=80)
 
+        top.config(bg='#55555a')
+
+        # Create a frame with gray background color
+        info_frame = Frame(top, bg='#55555a')
+        info_frame.pack(expand=True, fill='both', padx=20, pady=20)
+
+        # Add information labels (using Lorem Ipsum)
+        label1 = Label(info_frame, text="AUCTO Help Menu", fg='white',
+                          bg='#55555a',  font=("Helvetica", 14, "bold"))
+        label1.pack(pady=5)
+        label2 = Label(info_frame, text="Hello! If you have no idea how to use this program or what its for, you are in the right place.",
+                          fg='white', bg='#55555a')
+        label2.pack(pady=5)
+        label3 = Label(info_frame,
+                          text="This program is meant to store and backup information about \ndifferent companies your CTSO has partnered with!",
+                          fg='white', bg='#55555a')
+        label3.pack(pady=5)
+        label4 = Label(info_frame,
+                          text="Everything is stored in a directory of your choosing and a second secure location in your main drive\n and will show you all the files in that directory and all the folders too.",
+                          fg='white', bg='#55555a')
+        label4.pack(pady=5)
+        label5 = Label(info_frame,
+                          text="You can make new files of different types as well as look at and search through all of your current files.",
+                          fg='white', bg='#55555a')
+        label5.pack(pady=5)
+        label6 = Label(info_frame,
+                       text="To get started, click the button below and it will let you open your first directory.",
+                       fg='white', bg='#55555a')
+        label6.pack(pady=5)
+
+        # Add interactive button
+        btn_open = Button(top, text="Open Directory", command=self.open_and_close, bg='#55555a', fg='white', relief=FLAT)
+        btn_open.pack(pady=20)
+
+
+    def open_qa(self):
+        global q
+        q = Toplevel(root)
+        q.geometry("600x325")
+        q.resizable(False, False)
+        q.grab_set()
+        q.focus_set()
+        q.title("Help Menu")
+        Label(q, text="Hello World!", font=('Mistral 18 bold')).place(x=150, y=80)
+
+        q.config(bg='#55555a')
+
+        # Create a frame with gray background color
+        info_frame = Frame(q, bg='#55555a')
+        info_frame.pack(expand=True, fill='both', padx=20, pady=20)
+
+        # Add information labels (using Lorem Ipsum)
+        label1 = Label(info_frame, text="AUCTO Q&A/FAQ", fg='white',
+                          bg='#55555a',  font=("Helvetica", 14, "bold"))
+        label1.pack(pady=5)
+        label2 = Label(info_frame, text="Q: How do I open a file?",
+                          fg='white', bg='#55555a')
+        label2.pack(pady=5)
+        label3 = Label(info_frame,
+                          text="Once you opened a directory, double click one of your files to open it in the editing area.",
+                          fg='white', bg='#55555a')
+        label3.pack(pady=5)
+        label4 = Label(info_frame,
+                          text="Q: How do I open a directory in the program?",
+                          fg='white', bg='#55555a')
+        label4.pack(pady=5)
+        label5 = Label(info_frame,
+                          text="If you don't have a directory open you can use the 'File' menu to open\n a directory, or you can press 'Getting Started' in the Help menu.",
+                          fg='white', bg='#55555a')
+        label5.pack(pady=5)
+        label6 = Label(info_frame,
+                       text="Q: How do I search my information?",
+                       fg='white', bg='#55555a')
+        label6.pack(pady=5)
+        label7 = Label(info_frame,
+                       text="To search through all of the files in your current directory, you can use\n the 'Search Current Directory' command under the Window menu.",
+                       fg='white', bg='#55555a')
+        label7.pack(pady=5)
+
+
+    def open_inst(self):
+        global instruct
+        instruct = Toplevel(root)
+        instruct.geometry("600x325")
+        instruct.resizable(False, False)
+        instruct.grab_set()
+        instruct.focus_set()
+        instruct.title("Help Menu")
+        Label(instruct, text="Hello World!", font=('Mistral 18 bold')).place(x=150, y=80)
+
+        instruct.config(bg='#55555a')
+
+        # Create a frame with gray background color
+        info_frame = Frame(instruct, bg='#55555a')
+        info_frame.pack(expand=True, fill='both', padx=20, pady=20)
+
+        # Add information labels (using Lorem Ipsum)
+        label1 = Label(info_frame, text="AUCTO Step by Step Instructions", fg='white',
+                          bg='#55555a',  font=("Helvetica", 14, "bold"))
+        label1.pack(pady=5)
+        label2 = Label(info_frame, text="Step 1: Opening a Directory",
+                          fg='white', bg='#55555a')
+        label2.pack(pady=5)
+        label3 = Label(info_frame,
+                          text="In order to open a directory, you can either use the 'Open Directory' function\n in the File menu, or you can press 'Getting Started' in the Help menu.",
+                          fg='white', bg='#55555a')
+        label3.pack(pady=5)
+        label4 = Label(info_frame,
+                          text="Step 2: Making a New File",
+                          fg='white', bg='#55555a')
+        label4.pack(pady=5)
+        label5 = Label(info_frame,
+                          text="If you don't already have a file to edit, you can use the File menu\n to make a new file of the 3 different types avaliable.",
+                          fg='white', bg='#55555a')
+        label5.pack(pady=5)
+        label6 = Label(info_frame,
+                       text="Step 3: Editing Your Information",
+                       fg='white', bg='#55555a')
+        label6.pack(pady=5)
+        label7 = Label(info_frame,
+                       text="In order to edit your information, navigate through the tree system on\n the left side of the screen and double click on a file of your choosing.",
+                       fg='white', bg='#55555a')
+        label7.pack(pady=5)
+
+    def open_and_close(self):
+        # Run self.open()
+        self.open()
+
+        # Close the Toplevel window
+        top.destroy()
 
     def quit(self):
         self.checkFile()
